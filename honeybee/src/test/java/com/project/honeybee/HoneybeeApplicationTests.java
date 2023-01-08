520b56d5badde283567989711c0015ff05cc8f32
YSM <YSM@DESKTOP-UO9KU4N>
2023-01-08 12:41:27 +0900

    Start

---
+++ honeybee/src/test/java/com/project/honeybee/HoneybeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.honeybee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneybeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
